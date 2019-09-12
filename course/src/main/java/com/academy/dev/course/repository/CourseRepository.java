8e42654d96aef0cf84d2e39d91fa1f5b48c6b19c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-09-12 13:49:11 -0300

    Setup inicial microservices

---
+++ course/src/main/java/com/academy/dev/course/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.academy.dev.course.repository;
+
+import com.academy.dev.course.model.Course;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+
+}
